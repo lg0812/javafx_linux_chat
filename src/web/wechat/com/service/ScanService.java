bd1e2210e3d1fe3672592c3d508f5dce2070d044
LG0812 <235681512@qq.com>
2018-05-20 12:11:28 +0800

    none

--- src/web/wechat/com/service/ScanService.java
+++ src/web/wechat/com/service/ScanService.java
@@ -1,5 +1,7 @@
 package web.wechat.com.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -8,19 +10,20 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class ScanService {
-    StringBuilder jsLoginUrl = new StringBuilder("https://login.web.wechat.com/jslogin");
+    public static Log log = LogFactory.getLog(ScanService.class);
 
     public String jsLogin() {
+        StringBuilder jsLoginUrl = new StringBuilder("https://login.web.wechat.com/jslogin");
         CloseableHttpClient httpClient = HttpClients.createDefault();
         String str = "";
-        HttpGet httpGet = new HttpGet(jsLoginUrl.append("?appid=wx782c26e4c19acffb").
-                append("&redirect_uri=https://web.wechat.com/cgi-bin/mmwebwx-bin/webwxnewloginpage")
+        HttpGet httpGet = new HttpGet(jsLoginUrl.append("?appid=wx782c26e4c19acffb")
+                .append("&redirect_uri=https://web.wechat.com/cgi-bin/mmwebwx-bin/webwxnewloginpage")
                 .append("&fun=new")
                 .append("&lang=en_US")
                 .append("_=" + System.currentTimeMillis())
@@ -28,18 +31,49 @@ public class ScanService {
         try {
             Optional<CloseableHttpResponse> httpResponse = Optional.of(httpClient.execute(httpGet));
             HttpEntity httpEntity = httpResponse.get().getEntity();
-            str = EntityUtils.toString(httpEntity);
+            Map<String, String> map = getResponseData(EntityUtils.toString(httpEntity));
 
-            Pattern p = Pattern.compile("\".*\"");
-            Matcher m = p.matcher(str);
-            if (m.find()) {
-                str = m.group(0).replace("\"", "");
-            } else {
-                str = "";
-            }
+            str = map.get("window.QRLogin.uuid");
         } catch (IOException e) {
             e.printStackTrace();
         }
         return str;
+    }
+
+    public Map<String, String> login(String qrCodePostfix) {
+        StringBuilder loginUrl = new StringBuilder("https://login.web.wechat.com/cgi-bin/mmwebwx-bin/login");
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpGet httpGet = new HttpGet(loginUrl.append("?loginicon=true")
+                .append("&uuid=" + qrCodePostfix)
+                .append("&tip=0")
+                .append("&r=" + ~System.currentTimeMillis())
+                .append("&_" + System.currentTimeMillis())
+                .toString());
+        Map<String, String> map = new HashMap<>();
+        try {
+            Optional<CloseableHttpResponse> httpResponse = Optional.of(httpClient.execute(httpGet));
+            HttpEntity httpEntity = httpResponse.get().getEntity();
+            map = getResponseData(EntityUtils.toString(httpEntity));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("----->");
+        return map;
+    }
+
+
+    public Map<String, String> getResponseData(String response) {
+        log.info("response:" + response);
+        Map<String, String> map = new HashMap<>();
+        //不匹配  ;base64
+        String[] list = response.split(";(?!base64)");
+        for (String str : list) {
+            //split by first = symbol
+            String[] s = str.split("=", 2);
+            map.put(s[0].trim(), s[1].trim().replaceAll("\"|\'", ""));
+        }
+        log.info("result:" + map);
+        return map;
     }
 }
--- src/web/wechat/com/views/Scan.java
+++ src/web/wechat/com/views/Scan.java
@@ -3,19 +3,75 @@ package web.wechat.com.views;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
 import web.wechat.com.service.ScanService;
 
+import java.io.ByteArrayInputStream;
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Scan {
+
+    public static Log log = LogFactory.getLog(Scan.class);
     public ScanService scanService = new ScanService();
 
+
+    ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(10);
     @FXML
     public ImageView qrCode;
+
+    public String qrCodePostfix = "";
 
     public void initialize() {
         loadQrCode();
     }
 
     public void loadQrCode() {
-        qrCode.setImage(new Image("https://login.weixin.qq.com/qrcode/" + scanService.jsLogin().toString()));
+        qrCodePostfix = scanService.jsLogin().toString();
+        qrCode.setImage(new Image("https://login.weixin.qq.com/qrcode/" + qrCodePostfix));
+//        qrCode.setImage(new Image("https://www.baidu.com/img/baidu_jgylogo3.gif"));
+//        qrCode.setImage(new Image("https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/static/protocol/https/soutu/img/camera_new_5606e8f.png"));
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                Map<String, String> map = scanService.login(qrCodePostfix);
+//                System.out.println(map.toString());
+//                if ("201".equals(map.get("window.code"))) {
+//                    System.out.println("equal");
+//                    qrCode.setImage(new Image(new ByteArrayInputStream(Base64.getDecoder().decode(map.get("window.userAvatar").split(",")[1]))));
+////                    qrCode.setImage(new Image(new ByteArrayInputStream(Base64.getDecoder().decode(map.get("window.userAvatar")))));
+//                    timer.cancel();
+//                } else {
+//                    System.out.println("not equal");
+//                }
+//            }
+//
+//        }, 1000L);
+        scheduledExecutorService.scheduleWithFixedDelay(() -> {
+            Map<String, String> map = scanService.login(qrCodePostfix);
+            if ("201".equals(map.get("window.code"))) {
+                System.out.println(map.get("window.userAvatar").split(",")[1]);
+                qrCode.setImage(new Image(new ByteArrayInputStream(Base64.getDecoder().decode(map.get("window.userAvatar").split(",")[1]))));
+//                    qrCode.setImage(new Image(new ByteArrayInputStream(Base64.getDecoder().decode(map.get("window.userAvatar")))));
+//                scheduledExecutorService.shutdown();
+            } else if ("201".equals(map.get("window.code"))) {
+
+                scheduledExecutorService.shutdown();
+            } else {
+                log.info(map);
+                System.out.println("not equal");
+            }
+        }, 1, 1, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void tdddd() {
+        System.out.println("window.QRLogin.code = 200; window.QRLogin.uuid = \"wY4E_OFWQg==\"".replace("\"", ">"));
     }
 }
